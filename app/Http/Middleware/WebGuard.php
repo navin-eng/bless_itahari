c54b23b4c065c64d8a3dfa726a92ed2db951da60
navin-eng <navin@gplc.edu.np>
2026-09-25 09:02:05 +0545

    Fix admin panel caching issue by adding no-cache headers

--- app/Http/Middleware/WebGuard.php
+++ app/Http/Middleware/WebGuard.php
@@ -19,7 +19,11 @@ class WebGuard
     {
         if(Auth::check())
         {
-            return $next($request);
+            $response = $next($request);
+            $response->headers->set('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
+            $response->headers->set('Pragma', 'no-cache');
+            $response->headers->set('Expires', 'Sat, 01 Jan 2000 00:00:00 GMT');
+            return $response;
         }
         else
         {
